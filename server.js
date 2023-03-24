408b57146c8f061dc75f98b8b8591ad630ffdc88
hmzsjd <github@hamzasajid.com>
2023-03-24 23:34:45 +0300

    .

--- server.js
+++ server.js
@@ -1,41 +1,44 @@
 const express = require('express');
+const http = require('http');
 const WebSocket = require('ws');
 
-const app = express()
+const app = express();
 
-// Set up the socket.io server
-const server = app.listen(3000, () => {
-    console.log('Server listening on port 3000')
-});
+// Create an HTTP server instead of an HTTPS server
+const server = http.createServer(app);
 
-const io = require('socket.io')(server)
-app.locals.io = io
+// Initialize the Socket.IO server
+const io = require('socket.io')(server);
+app.locals.io = io;
 
+// Connect to the WebSocket endpoint over an insecure connection
 const wsClient = new WebSocket('wss://marketdata.tradermade.com/feedadv');
 
 wsClient.on('open', () => {
-    console.log('WebSocket client connected')
-  
-    wsClient.send('{"userKey":"wsPtsG_k-0EmfXy8W_PA", "symbol":"XAUUSD"}') // Send the subscription data to the server
-})
+  console.log('WebSocket client connected');
+  wsClient.send('{"userKey":"wsPtsG_k-0EmfXy8W_PA", "symbol":"XAUUSD"}');
+});
 
 wsClient.on('message', data => {
-    data = data.toString();
-    // console.log('Received data from websocket: ', data)
-    // Send the real-time data to the connected clients
-    app.locals.io.sockets.emit('update', data)
-})
-  
-// Handle incoming socket.io connections
-io.on('connection', socket => {
-    console.log('Socket connected:', socket.id)
+  data = data.toString();
+  console.log('Received data from WebSocket: ', data);
+  app.locals.io.sockets.emit('update', data);
+});
 
-    socket.on('disconnect', () => {
-        console.log('Socket disconnected:', socket.id)
-    });
-})
+// Handle incoming socket.io connections
+app.locals.io.on('connection', socket => {
+  console.log('Socket connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected:', socket.id);
+  });
+});
 
 // Set up the routes
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
